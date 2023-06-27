1664fc106e076bdba2f71c9c18bf323f5c0c2423
FilipposKontoes <fil13kon@gmail.com>
2023-06-27 17:48:52 +0300

    microservices creation

---
+++ cards/src/test/java/com/microservices/bankApp/BankAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.bankApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
