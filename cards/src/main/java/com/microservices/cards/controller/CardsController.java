134e3240a840eb569ac3a3728ed397c36d31f1d1
FilipposKontoes <fil13kon@gmail.com>
2023-07-06 10:34:52 +0300

    cards microservice

---
+++ cards/src/main/java/com/microservices/cards/controller/CardsController.java
@@ -0,0 +1,36 @@
+package com.microservices.cards.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.microservices.cards.model.Cards;
+import com.microservices.cards.model.Customer;
+import com.microservices.cards.repository.CardsRepository;
+
+/**
+ * @author Eazy Bytes
+ *
+ */
+
+@RestController
+public class CardsController {
+
+	@Autowired
+	private CardsRepository cardsRepository;
+
+	@PostMapping("/myCards")
+	public List<Cards> getCardDetails(@RequestBody Customer customer) {
+		List<Cards> cards = cardsRepository.findByCustomerId(customer.getCustomerId());
+		if (cards != null) {
+			return cards;
+		} else {
+			return null;
+		}
+
+	}
+
+}
