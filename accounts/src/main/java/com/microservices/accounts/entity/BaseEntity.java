f87d212b2e47f0d5db94bbb8e64e30cbb9940a10
FilipposKontoes <fil13kon@gmail.com>
2023-10-11 23:54:42 +0300

    import fixes

--- accounts/src/main/java/com/microservices/accounts/entity/BaseEntity.java
+++ accounts/src/main/java/com/microservices/accounts/entity/BaseEntity.java
@@ -2,11 +2,8 @@ package com.microservices.accounts.entity;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.MappedSuperclass;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import jakarta.persistence.*;
+import lombok.*;
 
 @MappedSuperclass
 @Getter @Setter @ToString
