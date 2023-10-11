81a01115ace590afe2fa7a5fbb97c79652225f35
FilipposKontoes <fil13kon@gmail.com>
2023-10-11 23:55:39 +0300

    controller fixes

--- accounts/src/main/java/com/microservices/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/microservices/accounts/controller/AccountsController.java
@@ -5,26 +5,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.microservices.accounts.model.Accounts;
-import com.microservices.accounts.model.Customer;
+import com.microservices.accounts.entity.Accounts;
+import com.microservices.accounts.entity.Customer;
 import com.microservices.accounts.repository.AccountsRepository;
 
 @RestController
 public class AccountsController {
-
-	@Autowired
-	private AccountsRepository accountsRepository;
-
-	@PostMapping("/myAccount")
-	public Accounts getAccountDetails(@RequestBody Customer customer) {
-
-		Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
-		if (accounts != null) {
-			return accounts;
-		} else {
-			return null;
-		}
-
-	}
 
 }
